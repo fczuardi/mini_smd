d86b5b59c6486fe221ff3d9491b748ece7b600fa
Fabricio Zuardi <fabricio@fabricio.org>
2012-12-27 16:19:56 -0200

    desabilitei o cache-busting do jquery para melhorar funcionamento offline

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: text/cache-manifest');
 ?>CACHE MANIFEST
-# 2012-12-27:v1
+# 2012-12-27:v2
 
 CACHE:
 1_identificacao.html
@@ -16,13 +16,36 @@ app.html
 list.html
 css/my.css
 css/jquery.mobile-1.2.0.min.css
-images/ajax-loader.gif
-images/icons-18-black.png
-images/icons-18-white.png
-images/icons-36-black.png
-images/icons-36-white.png
+css/images/ajax-loader.gif
+css/images/icons-18-black.png
+css/images/icons-18-white.png
+css/images/icons-36-black.png
+css/images/icons-36-white.png
 img/capaMiniSMD.png
-js/
+js/custom-datepicker.js
+js/jquery-1.8.2.min.js
+js/jquery.mobile-1.2.0.min.js
+js/load_polyfills.js
+js/main.js
+js/polyfills/jquery.json-2.4.min.js
+js/polyfills/jstorage.js
+mobpick/external/jquery-1.6.4.min.js
+mobpick/external/jquery.mobile-1.0.js
+mobpick/external/jquery.mobile-1.0.min.css
+mobpick/external/jquery.mobile-1.0.min.js
+mobpick/external/modernizr.custom.min.js
+mobpick/external/qunit.css
+mobpick/external/qunit.js
+mobpick/external/xdate.i18n.js
+mobpick/external/xdate.js
+mobpick/external/images/ajax-loader.png
+mobpick/external/images/icons-18-black.png
+mobpick/external/images/icons-18-white.png
+mobpick/external/images/icons-36-black.png
+mobpick/external/images/icons-36-white.png
+mobpick/js/mobipick.js
+mobpick/css/mobipick.css
+mobpick/css/images/clock.png
 
 NETWORK:
 sync.php
