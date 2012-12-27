f09177569246a68c3b076530a520473699fa5516
Fabricio Zuardi <fabricio@fabricio.org>
2012-12-27 16:03:03 -0200

    primeiro teste com appcache

---
+++ manifest.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-type: text/cache-manifest');
+?>CACHE MANIFEST
+# 2012-12-27:v1
+
+CACHE:
+1_identificacao.html
+2_coop-wonca.html
+3_phq-2.html
+4_gad-2.html
+5_idate-3.html
+6_cudit-3.html
+7_m-3.html
+8_apss-3.html
+app.html
+list.html
+css/my.css
+css/jquery.mobile-1.2.0.min.css
+images/ajax-loader.gif
+images/icons-18-black.png
+images/icons-18-white.png
+images/icons-36-black.png
+images/icons-36-white.png
+img/capaMiniSMD.png
+js/
+
+NETWORK:
+sync.php
